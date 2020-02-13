5b5631ec1de4227030681d547a1458c4a8bf435c
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-02-13 13:34:23 -0300

    formulario terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,35 @@ function validaDatos() {
     var telefono = document.getElementById("telefono").value;
 
     if (nombre == "" || apellidos == "" || correo == "" || usuario == "" || contrasenia == "" || telefono == "") {
-        alert("Debes completar todos los campos");
+        alert("Existen datos vacios");
         return false;
     }
-    if (nombre.length() > 20){
+    if (nombre.length > 20){
         alert("El nombre es muy largo");
         return false;
     }
-        alert("Bienvenido(a) " + nombre + " " + apellidos + "!  \n Tu usuario es: " + usuario + "\n Tu contrase\361a es: " + contrasenia);
+    if (apellidos.length > 80) {
+        alert("El apellido es muy largo");
+        return false;
+    }
+    if (correo.length > 100) {
+        alert("El correo es muy largo");
+        return false;
+    }
+    if (usuario.length > 20) {
+        alert("El usuario es muy largo");
+        return false;
+    }
+    if (contrasenia.length > 20) {
+        alert("La contraseña es muy larga");
+        return false;
+    }
+    if (telefono.length > 15) {
+        alert("El telefono es muy largo");
+        return false;
+    }
+    document.getElementById("formularioRegistro").submit();
+    alert("Bienvenido(a) " + nombre + " " + apellidos + "!  \n Tu usuario es: " + usuario + "\n Tu contrase\361a es: " + contrasenia);
 
 }
 
